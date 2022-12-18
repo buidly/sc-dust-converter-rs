0fc0480cd2138908694d32c8e1e09c7cf4daca78
alexjilavu <alexjilavu17@gmail.com>
2022-12-18 13:50:55 +0200

    implement config and proxy module

--- dust-converter/src/lib.rs
+++ dust-converter/src/lib.rs
@@ -2,9 +2,16 @@
 
 elrond_wasm::imports!();
 
+pub mod config;
+pub mod proxy;
+
 /// An empty contract. To be used as a template when starting a new contract from scratch.
 #[elrond_wasm::contract]
-pub trait EmptyContract {
+pub trait EmptyContract:
+    config::ConfigModule +
+    proxy::ProxyModule 
+{
+    
     #[init]
     fn init(&self) {}
 }

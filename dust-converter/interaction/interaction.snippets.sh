a2f8f3ca6d44697aa4a7e016e57d62579a2ade6a
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2023-02-06 12:39:49 +0200

    ledger-interactions

--- dust-converter/interaction/interaction.snippets.sh
+++ dust-converter/interaction/interaction.snippets.sh
@@ -1,8 +1,13 @@
 WALLET_PEM="../../../dev-wallet/main1.pem"
-PROXY="https://devnet-gateway.elrond.com"
+PROXY="https://devnet-gateway.multiversx.com"
 CHAIN_ID="D"
 
-DUST_CONVERTER_ADDRESS="erd1qqqqqqqqqqqqqpgq38zwarrn24c6df4essae6k8xeywf7aaa4jusz203xk"
+DUST_CONVERTER_ADDRESS="erd1qqqqqqqqqqqqqpgq6u07hhkfsvuk5aae92g549s6pc2s9ycq0dps368jr5"
+
+
+SLIPPAGE=50
+FEE_PERCENTAGE=500
+WEGLD=WEGLD-bd4d79
 
 # . ./interaction.snippets.sh && deploy 500 100 WEGLD-d7c6bb
 deploy() {
@@ -10,11 +15,11 @@ deploy() {
 
     erdpy --verbose contract deploy --recall-nonce \
         --metadata-payable-by-sc \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --gas-limit=200000000 \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --bytecode="../output/dust-converter.wasm" \
-        --arguments $1 $2 $wegld_token_id \
+        --arguments ${FEE_PERCENTAGE} ${SLIPPAGE} str:${WEGLD} \
         --outfile="deploy-stake-farm-internal.interaction.json" --send || return
 
     ADDRESS=$(erdpy data parse --file="deploy-stake-farm-internal.interaction.json" --expression="data['contractAddress']")
@@ -25,15 +30,13 @@ deploy() {
 
 # . ./interaction.snippets.sh && upgrade 500 100 WEGLD-d7c6bb
 upgrade() {
-    wegld_token_id="0x$(echo -n $3 | xxd -p -u | tr -d '\n')"
-
     erdpy --verbose contract upgrade ${DUST_CONVERTER_ADDRESS} --recall-nonce \
         --metadata-payable-by-sc \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --gas-limit=200000000 \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --bytecode="../output/dust-converter.wasm" \
-        --arguments $1 $2 $wegld_token_id \
+        --arguments ${FEE_PERCENTAGE} ${SLIPPAGE} str:${WEGLD} \
         --outfile="deploy-stake-farm-internal.interaction.json" --send || return
 
     ADDRESS=$(erdpy data parse --file="deploy-stake-farm-internal.interaction.json" --expression="data['contractAddress']")
@@ -44,10 +47,19 @@ upgrade() {
 
 resume() {
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=30000000 \
         --function=resume \
+        --send || return
+}
+
+extractFees() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=extractFees \
         --send || return
 }
 
@@ -60,7 +72,7 @@ addKnownTokens() {
     token_id="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
 
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=10000000 \
         --function=addKnownTokens \
@@ -73,7 +85,7 @@ removeKnownTokens() {
     token_id="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
 
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=10000000 \
         --function=removeKnownTokens \
@@ -84,7 +96,7 @@ removeKnownTokens() {
 # . ./interaction.snippets.sh && sellDustTokens
 sellDustTokens() {
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=30000000 \
         --function=sellDustTokens \
@@ -94,7 +106,7 @@ sellDustTokens() {
 registerReferralTag() {
     tag="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=30000000 \
         --function=registerReferralTag \
@@ -102,13 +114,24 @@ registerReferralTag() {
         --send || return
 }
 
-# . ./interaction.snippets.sh && addTierDetails Bronze 0x00 500 Silver 0x06f05b59d3b20000 1000 Gold 0x0a688906bd8b0000 2000
-# . ./interaction.snippets.sh && addTierDetails Silver 0x06f05b59d3b20000 1000
+# . ./interaction.snippets.sh && addTierDetails Bronze 0x00 500 Silver 0x22b1c8c1227a0000 1000 Gold 0xd02ab486cedc0000 2000
+# . ./interaction.snippets.sh && addTierDetails Platinum 0x056bc75e2d63100000 4000
 # . ./interaction.snippets.sh && addTierDetails Gold 0x0a688906bd8b0000 2000
 # . ./interaction.snippets.sh && addTierDetails Platinum 0x0de0b6b3a7640000 3000
 addTierDetails() {
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=addTierDetails \
+        --arguments str:$1 $2 $3 \
+        --send || return
+}
+
+
+addTierDetails() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=10000000 \
         --function=addTierDetails \
@@ -119,10 +142,74 @@ addTierDetails() {
 # . ./interaction.snippets.sh && removeTierDetails Silver 
 removeTierDetails() {
     erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
-        --pem=${WALLET_PEM} \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
         --gas-limit=10000000 \
         --function=removeTierDetails \
         --arguments str:$1 \
+        --send || return
+}
+
+setFeePercentage() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=setFeePercentage \
+        --arguments $FEE_PERCENTAGE \
+        --send || return
+}
+
+TAG=egld4ever
+CUSTOM_FEE=2000
+setReferralFeePercentage() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=setReferralFeePercentage \
+        --arguments str:${TAG} $CUSTOM_FEE \
+        --send || return
+}
+
+resume() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=resume \
+        --send || return
+}
+
+pause() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=pause \
+        --send || return
+}
+
+
+# . ./interaction.snippets.sh && removeKnownTokens RIDE-6e4c49
+removeKnownTokens() {
+    token_id="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
+
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=10000000 \
+        --function=removeKnownTokens \
+        --arguments $token_id \
+        --send || return
+}
+
+# . ./interaction.snippets.sh && sellDustTokens
+sellDustTokens() {
+    erdpy --verbose contract call ${DUST_CONVERTER_ADDRESS} --recall-nonce \
+        --ledger --ledger-account-index 0 --ledger-address-index 4  \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=30000000 \
+        --function=sellDustTokens \
         --send || return
 }
